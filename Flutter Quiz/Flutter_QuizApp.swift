66790727dff0a04eeb5fd858d975f65e8bccf4ae
Totskiy Pavel <tockiypav@icloud.com>
2025-01-23 16:48:41 +0500

    Initial Commit

---
+++ Flutter Quiz/Flutter_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flutter_QuizApp.swift
+//  Flutter Quiz
+//
+//  Created by Pavel Totskiy on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Flutter_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
